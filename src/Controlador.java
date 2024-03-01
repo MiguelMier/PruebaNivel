db3baa8fa819f896415a6d33239da9bd81f94fe4
Miguel Mier <miguel.mier@babelgroup.com>
2024-03-01 10:49:22 +0100

    pequeños cambios clean code / pequeño arreglo

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,46 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Controlador {
 
     private int puntuacion;
     private String contrasena;
-    private Boolean mayusminus;
 
-    public Controlador(){
+    public Controlador() {
         this.puntuacion = 0;
-        this.mayusminus = false;
     }
 
-    public void start(){
+    public void start() {
         leerContrasena();
         calcularPuntuacion();
         permitirAcceso();
     }
 
-    private void leerContrasena(){
+    private void leerContrasena() {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Intrioduzca su nombre:");
+        System.out.println("Introduzca su nombre:");
         String nombre = teclado.nextLine();
-        System.out.println("Intrioduzca su contraseña " + nombre + ":");
+        System.out.println("Introduzca su contraseña " + nombre + ":");
         contrasena = teclado.nextLine();
-        contrasena = contrasena.replace(" ","");
-
+        contrasena = contrasena.replace(" ", "");
     }
 
-    private void calcularPuntuacion(){
+    private void calcularPuntuacion() {
         calculaLongitud();
-        numeros();
-        letrasMinusMayus();
-        calculaUsoEspeciales();
-        puntoExtra();
+        puntuacionNumeros();
+        puntuacionLetrasMinusMayus();
+        puntuacionEspeciales();
+        puntuacionExtra();
     }
 
-    private void calculaLongitud(){
+    private void calculaLongitud() {
         int longitud = contrasena.length();
 
-        if(longitud == 7 || longitud == 8){
+        if (longitud == 7 || longitud == 8) {
             puntuacion++;
         } else if (longitud >= 9 && longitud <= 12) {
             puntuacion += 2;
@@ -49,56 +44,56 @@ public class Controlador {
         }
     }
 
-    private void numeros(){
+    private void puntuacionNumeros() {
         boolean tempNum = false;
-         for (int i = 0; i < contrasena.length(); i++){
+        for (int i = 0; i < contrasena.length(); i++) {
             if (contrasena.substring(i).matches("[0-9]")) {
                 tempNum = true;
             }
         }
-        if(tempNum){
+        if (tempNum) {
             puntuacion++;
         }
-
     }
 
-    private void letrasMinusMayus(){
+    private void puntuacionLetrasMinusMayus() {
         boolean tempLetra = false;
         boolean tempMayus = false;
-        for (int i = 0; i < contrasena.length(); i++){
-            if(contrasena.substring(i).matches("[a-z]")){
+
+        for (int i = 0; i < contrasena.length(); i++) {
+            char caracter = contrasena.charAt(i);
+            if (Character.isLowerCase(caracter)) {
                 tempLetra = true;
-            } else if (contrasena.substring(i).matches("[A-Z]")) {
+            } else if (Character.isUpperCase(caracter)) {
                 tempMayus = true;
             }
         }
-        if(tempLetra || tempMayus){
+
+        if (tempLetra || tempMayus) {
             puntuacion++;
         }
-        if(tempLetra && tempMayus){
-            puntuacion += 2;
-        }
 
-    }
-
-    private void calculaUsoEspeciales(){
-        if(!contrasena.matches("^[a-zA-Z0-9]*$")){
+        if (tempLetra && tempMayus) {
             puntuacion += 2;
         }
     }
+    private void puntuacionEspeciales() {
+        if (!contrasena.matches("^[a-zA-Z0-9]*$")) {
+            puntuacion += 2;
+        }
+    }
 
-    private void puntoExtra(){
-        if(puntuacion == 9){
+    private void puntuacionExtra() {
+        if (puntuacion == 9) {
             puntuacion++;
         }
     }
 
-    private void permitirAcceso(){
-        if(puntuacion >= 8){
+    private void permitirAcceso() {
+        if (puntuacion >= 8) {
             System.out.println("Acceso permitido");
             System.out.println("Su puntuación de contraseña es: " + puntuacion);
-        }
-        else {
+        } else {
             System.out.println("Acceso denegado");
             System.out.println("Su puntuación de contraseña es: " + puntuacion);
         }
